8383410cdf55fa8ee836af0b4e7879e96758f19e
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-06-29 16:05:31 +0300

    finish the part 1 step 4 - Dictionary Lookup

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
-# part 1
-# step 1 - Age Converter
-try:
-    age = input("Enter your age: ")
-    next_year = int(age) + 1
-    print("Next year you will be", next_year)
-except ValueError:
-    print("Age must be a number")
+# # part 1
+# # step 1 - Age Converter
+# try:
+#     age = input("Enter your age: ")
+#     next_year = int(age) + 1
+#     print("Next year you will be", next_year)
+# except ValueError:
+#     print("Age must be a number")
 
-# step 2 - Safe Division
+# # step 2 - Safe Division
+# try:
+#     a = int(input("First number: "))
+#     b = int(input("Second number: "))
+#     print(a / b)
+# except ZeroDivisionError:
+#     print("Cannot divide by zero")
+# # step 3 - Number From List
+# try:
+#     numbers = [10, 20, 30]
+#     index = int(input("Choose index: "))
+#     print(numbers[index])
+# except IndexError:
+#     print("Index not found")
+# step 4 - Dictionary Lookup
+prices = {
+    "apple": 3,
+    "banana": 5
+}
 try:
-    a = int(input("First number: "))
-    b = int(input("Second number: "))
-    print(a / b)
-except ZeroDivisionError:
-    print("Cannot divide by zero")
-# step 3 - Number From List
-try:
-    numbers = [10, 20, 30]
-    index = int(input("Choose index: "))
-    print(numbers[index])
-except IndexError:
-    print("Index not found")
+    item = input("Enter item: ")
+    print(prices[item])
+except KeyError:
+    print("Item not found")
